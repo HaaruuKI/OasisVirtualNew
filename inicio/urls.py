d4df2ee6b35a78def9b146672c1c1755f809c468
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-06-19 00:11:51 -0700

    Update navigation section and add event listeners for navigation

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from inicio import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
     path('login/', views.log_in, name="login"),
     path('datails/', views.details_game, name="details"),
 
